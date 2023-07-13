ea20dbc7abfbd447d92a68c24d856b94a50de79a
Nonyito <nonyitobrighton@gmail.com>
2023-07-13 15:30:29 -0700

    puts print out

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* main is Entry point
+ *
+ * Retrun (0) Always (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
