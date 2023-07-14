3d78daa9fe30ac98cda967eb5aafc4c4ec44f9c1
Nonyito <nonyitobrighton@gmail.com>
2023-07-14 02:48:27 -0700

    using puts to print out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print Message
  *
- * Retrun: Always 0 (Success)
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
