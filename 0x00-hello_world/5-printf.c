9778cf4843b2d029551b6828cecc3843fafc3533
Nonyito <nonyitobrighton@gmail.com>
2023-07-15 15:28:15 -0700

    printing text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the massage using char and printf
+ *
+ * Description: the function print the text using char and printf
+ * Return: its's always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
