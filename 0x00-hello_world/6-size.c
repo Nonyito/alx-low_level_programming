c31ca073565aa3497c12ae2e02b4a757a90537ff
Nonyito <nonyitobrighton@gmail.com>
2023-07-15 16:01:03 -0700

    printing various system var size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,4 +14,6 @@ int main(void)
 	printf("Size of long is %d byte(s)\n", sizeof(long));
 	printf("Size of long long is %d byte(s)\n", sizeof(long long));
 	printf("Size of float is %d byte(s)\n", sizeof(float));
+
+	return (0);
 }
