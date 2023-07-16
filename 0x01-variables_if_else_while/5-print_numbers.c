908a240763d1cb1fd235a34109f0739c282e8d74
Nonyito <nonyitobrighton@gmail.com>
2023-07-16 10:30:29 -0700

    printing base of 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop inside
+ *
+ * Description: it print all the base of 10
+ * Return: it is always 0
+ */
+
+int main(void)
+{
+	for (n = 0, n < 10, n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
