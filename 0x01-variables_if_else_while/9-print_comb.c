41332e3a41c428c5f63e4822d5502efb793ef985
Nonyito <nonyitobrighton@gmail.com>
2023-07-16 13:45:49 -0700

    printing 0 to 9 in array form

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - fors loops and if
+ *
+ * Description: it list the nam from 0 to 9 like array
+ * Return: it is always 0
+ */
+
+int main(void)
+{
+	char i, n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (n = i; n <= '9'; n++)
+		{
+			putchar(i);
+			putchar(n);
+			if (i != '9' || n != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
