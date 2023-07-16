e90d532a8a4350f82d804743176d8c520a349414
Nonyito <nonyitobrighton@gmail.com>
2023-07-16 10:57:33 -0700

    print the base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - carry for loop
+ *
+ * Description: its a for loop it prints the base of 10
+ * Return: it is always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
