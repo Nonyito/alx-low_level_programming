5370d4ffa1459b67f49811a1fcaa40a20be17a32
Nonyito <nonyitobrighton@gmail.com>
2023-07-18 12:59:03 -0700

    alphabet a to z print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - while loop
+ *
+ * Description: prototype, while loop printing alphabet
+ * Return: it is always 0
+ */
+
+void print_alphabet(void)
+{
+	char let = 'a';
+
+	while (let < 'z')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar("\n");
+	return (0);
+}
