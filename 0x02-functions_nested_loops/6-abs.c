e4423811cd5dbd5b2394fa57815d822ab16d3bdc
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 15:19:30 -0700

    .

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,14 +4,15 @@
 /**
  * _abs - find absolute point
  *
- * Return: 
+ * @num: the int value
+ * Return: it is 0
  */
 
 int _abs(int num)
 {
 	if (num < 0)
 	{
-		num = (-1) * num;
+		num = -1 * num;
 		_putchar(num);
 	}
 	else
