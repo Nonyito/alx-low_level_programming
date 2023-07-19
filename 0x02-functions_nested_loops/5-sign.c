3c34e9df3e29a38b2764b8f6d4e5024378aa1c7d
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 13:31:59 -0700

    printing - to + due to input vr01

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf('+');
+		putchar(+'\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf(0);
+		putchar(0'\n');
 		return (0);
 	}
 	else
 	{
-		printf('-');
+		putchar(-'\n');
 		return (-1);
 	}
 }
