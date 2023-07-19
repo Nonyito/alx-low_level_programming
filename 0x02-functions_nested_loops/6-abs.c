acba62ea7aa0b79bec33685908777cfd07f00461
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 15:05:30 -0700

    absolute of any number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - find absolute point
+ *
+ * Return: 
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = (-1) * int;
+		_putchar(int);
+	}
+	else
+	{
+		_putchar(int);
+	}
+}
