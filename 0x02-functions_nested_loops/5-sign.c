4aeef213297685df5a5d3f235c2b9154210fb99a
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 13:56:37 -0700

    three ifss

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar("+");
+		putchar('+');
+		putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar("0");
+		putchar('0');
+		putchar('\n');
 		return (0);
 	}
 	else
 	{
-		putchar("-");
+		putchar('-');
+		putchar('\n');
 		return (-1);
 	}
 }
