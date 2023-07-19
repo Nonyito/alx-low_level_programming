cddcded2774625d8ffca7e554a95d2a6078f22eb
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 14:00:36 -0700

    three state

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		putchar('0');
-		putchar('\n');
 		return (0);
 	}
 	else
 	{
 		putchar('-');
-		putchar('\n');
 		return (-1);
 	}
 }
