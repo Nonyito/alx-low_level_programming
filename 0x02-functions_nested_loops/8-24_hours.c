078fbca64670f747aeb5d6ef15c6c69c13d6f1d3
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 17:48:46 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - clock 24hrs list
+ *
+ * Return: non
+ */
+
+void jack_bauer(void)
+{
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mns = 0; mns < 60; mns++)
+		{
+			_putchar("%02d:%02d\n", hrs, mns);
+		}
+	}
+}
