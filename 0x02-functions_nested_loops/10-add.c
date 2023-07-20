f7ec12e17bca13353e6cfeaeddc5efe2a006560d
Nonyito <nonyitobrighton@gmail.com>
2023-07-19 17:05:07 -0700

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adding to int
+ *
+ * @num1: first num input
+ *
+ * @num2: second num input
+ *
+ * Return: 0
+ */
+
+int add(int num1, int num2)
+{
+	int anw;
+
+	anw = num1 + num2;
+	_putchar(anw);
+	return (0);
+}
