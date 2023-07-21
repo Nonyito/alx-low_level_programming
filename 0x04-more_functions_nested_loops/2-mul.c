e2282415546dccd12da95bde916edb05802b3cfe
Nonyito <nonyitobrighton@gmail.com>
2023-07-21 11:45:50 -0700

    mul to int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - mult two int
+ *
+ * @a: first int
+ * @b: second int
+ * Return: is anwser
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
