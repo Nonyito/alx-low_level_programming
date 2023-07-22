dc86f254815e313a45b8fe8b0c849bfe372188bb
Nonyito <nonyitobrighton@gmail.com>
2023-07-21 17:51:40 -0700

    .

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - printng number
@@ -11,9 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i > 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar('0' + i);
+		_putchar('0' + i);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
