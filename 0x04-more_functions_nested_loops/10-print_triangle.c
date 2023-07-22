89f76f0a3df675984aa3bc5f014b90dfdc3deb6c
Nonyito <nonyitobrighton@gmail.com>
2023-07-21 20:01:33 -0700

    last tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - printing trianlges
+ *
+ * @sizi: input value
+ * Return: is 0
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, n;
+
+		for (i = 0; i < size; i++)
+		{
+			for (n = 0; n <= i; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
