b6100ae4c8193f16cca2e5d7bae8071864910706
Nonyito <nonyitobrighton@gmail.com>
2023-07-21 18:18:00 -0700

    printing tens times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - ten times 1 to 14 printing
+ *
+ * Return: is void
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
