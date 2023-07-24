f3c13ffacad6f654b8ce6aab00573149aaf32228
Nonyito <nonyitobrighton@gmail.com>
2023-07-24 16:36:48 -0700

    printing each line of latter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - printing each line in new line
+ *
+ * @str: the input character
+ * Return: it is a void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; 1 += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
