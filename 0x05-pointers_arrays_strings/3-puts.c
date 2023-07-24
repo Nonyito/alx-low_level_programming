76a5e7d883a22824e70c8bf0a4aad3fd68571fdc
Nonyito <nonyitobrighton@gmail.com>
2023-07-24 12:53:17 -0700

    printing string pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - printing a string
+ *
+ * @str: output string
+ * Return: is void
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
