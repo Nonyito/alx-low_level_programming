2ea5250257cee4c629054e16008356184eb75812
Nonyito <nonyitobrighton@gmail.com>
2023-07-27 14:11:41 -0700

    copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copying a string
+ *
+ * @*dest: input string
+ * @*str: input string
+ * @n: number
+ * Return: it is l
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return dest;
+}
