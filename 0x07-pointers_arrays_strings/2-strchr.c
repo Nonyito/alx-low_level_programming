ba98fd81392d22349568074b117ac7138b778bf5
Nonyito <nonyitobrighton@gmail.com>
2023-08-01 14:07:30 -0700

    .

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - finding a string in character
+ *
+ * @c: returns a pointer
+ * @s: it is not null
+ * Return: it is NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
