8eb32320e471bb05adb1427f3f2a10c81a102cdd
Nonyito <nonyitobrighton@gmail.com>
2023-08-09 09:50:16 -0700

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,6 +10,13 @@
 
 int main(int argc, char* argv[])
 {
-	_putchar("%d\n", argv[0]);
+	char *proname = argv[0];
+
+	while (*proname != '\0')
+	{
+		_putchar(*proname);
+		proname++;
+	}
+	_putchar('\n');
 	return (0);
 }
