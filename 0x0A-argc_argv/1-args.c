f93875699bfc29b68be4aff3229cbd03c9fcf60e
Nonyito <nonyitobrighton@gmail.com>
2023-08-09 11:01:34 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: the numbers of args entered
+ * @argv: the array of args availble
+ * Return: it is 0
+ */
+
+int main(int argc, char* argv)
+{
+	_putchar("%d\n", argc);
+	return (0);
+}
