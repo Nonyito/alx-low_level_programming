dc8cdf755f9073220dc7ae9246126726ceb8fdc3
Nonyito <nonyitobrighton@gmail.com>
2023-08-17 13:43:09 -0700

    Im back from the dead man

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_name - print a name
+ *
+ * @name: name print
+ * @f: input print
+ * Return: its a null
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		_putchar(name[i]);
+	}
+	f(name);
+}
